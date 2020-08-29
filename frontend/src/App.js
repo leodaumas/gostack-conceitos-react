5e7e4b66613d27b59ce444ae9fadc048e8f62d4c
leodaumas <leofdaumas@gmail.com>
2020-08-29 20:31:31 +0200

    Likes implementados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ async    function handleAddProject() {
 //        setProjects([...projects, `Novo projeto ${Date.now()}`]);
         const response = await api.post('projects', {
             title: `Novo projeto ${Date.now()}`,
-            owner: "Diego Fernandes"
+            owner: "Leo"
         });
 
         const project = response.data;
